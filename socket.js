905424696c2a536f4c8653c621aca35c2ac00b3b
Shahina858 <mshahina480@gmail.com>
2026-02-21 17:32:45 +0530

    Fix CORS and Socket.IO config for Netlify frontend

--- socket.js
+++ socket.js
@@ -4,7 +4,7 @@ import { Server } from "socket.io";
 export const initSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://magical-melba-48b87a.netlify.app",
       methods: ["GET", "POST"],
       credentials: true,
     },
